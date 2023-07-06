2cf56fb7292fce26e07f2912e1b85d00db8b125b
Ankit Singh <138468487+ankitsi001@users.noreply.github.com>
2023-07-06 17:51:20 +0530

    Update hi.php
    calc

--- hi.php
+++ hi.php
@@ -1,62 +1,100 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Calculator</title>
-  <style>
-    .calculator {
-      width: 200px;
-      padding: 10px;
-      background-color: #f2f2f2;
-    }
-    .calculator input[type="button"] {
-      width: 100%;
-      padding: 5px;
-      margin-bottom: 5px;
-    }
-    .calculator input[type="text"] {
-      width: 100%;
-      padding: 5px;
-      margin-bottom: 10px;
-    }
-  </style>
+    <title>Calculator</title>
+    <style>
+        .calculator {
+            width: 200px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        .calculator input {
+            width: 100%;
+            margin-bottom: 10px;
+            padding: 5px;
+        }
+
+        .calculator .row {
+            display: flex;
+        }
+
+        .calculator .row button {
+            flex: 1;
+            margin: 2px;
+            padding: 10px;
+            font-size: 16px;
+            border-radius: 5px;
+            border: none;
+            cursor: pointer;
+        }
+
+        .calculator .row button.operator {
+            background-color: #ff9800;
+            color: white;
+        }
+
+        .calculator .row button.clear {
+            background-color: #f44336;
+            color: white;
+        }
+
+        .calculator .row button.equal {
+            background-color: #4caf50;
+            color: white;
+        }
+    </style>
 </head>
 <body>
+    <div class="calculator">
+        <input type="text" id="result" readonly>
+        <div class="row">
+            <button onclick="addToResult(7)">7</button>
+            <button onclick="addToResult(8)">8</button>
+            <button onclick="addToResult(9)">9</button>
+            <button onclick="addToResult('/')">/</button>
+        </div>
+        <div class="row">
+            <button onclick="addToResult(4)">4</button>
+            <button onclick="addToResult(5)">5</button>
+            <button onclick="addToResult(6)">6</button>
+            <button onclick="addToResult('*')">*</button>
+        </div>
+        <div class="row">
+            <button onclick="addToResult(1)">1</button>
+            <button onclick="addToResult(2)">2</button>
+            <button onclick="addToResult(3)">3</button>
+            <button onclick="addToResult('-')">-</button>
+        </div>
+        <div class="row">
+            <button onclick="addToResult(0)">0</button>
+            <button onclick="addToResult('.')">.</button>
+            <button onclick="calculate()">=</button>
+            <button onclick="addToResult('+')">+</button>
+        </div>
+        <div class="row">
+            <button onclick="clearResult()" class="clear">Clear</button>
+        </div>
+    </div>
 
-<div class="calculator">
-  <input type="text" id="result" readonly>
-  <input type="button" value="1" onclick="appendToResult('1')">
-  <input type="button" value="2" onclick="appendToResult('2')">
-  <input type="button" value="3" onclick="appendToResult('3')">
-  <input type="button" value="+" onclick="appendToResult('+')">
-  <input type="button" value="4" onclick="appendToResult('4')">
-  <input type="button" value="5" onclick="appendToResult('5')">
-  <input type="button" value="6" onclick="appendToResult('6')">
-  <input type="button" value="-" onclick="appendToResult('-')">
-  <input type="button" value="7" onclick="appendToResult('7')">
-  <input type="button" value="8" onclick="appendToResult('8')">
-  <input type="button" value="9" onclick="appendToResult('9')">
-  <input type="button" value="*" onclick="appendToResult('*')">
-  <input type="button" value="C" onclick="clearResult()">
-  <input type="button" value="0" onclick="appendToResult('0')">
-  <input type="button" value="=" onclick="calculateResult()">
-  <input type="button" value="/" onclick="appendToResult('/')">
-</div>
+    <script>
+        function addToResult(value) {
+            document.getElementById("result").value += value;
+        }
 
-<script>
-  function appendToResult(value) {
-    document.getElementById('result').value += value;
-  }
+        function calculate() {
+            try {
+                const result = eval(document.getElementById("result").value);
+                document.getElementById("result").value = result;
+            } catch (error) {
+                document.getElementById("result").value = "Error";
+            }
+        }
 
-  function clearResult() {
-    document.getElementById('result').value = '';
-  }
-
-  function calculateResult() {
-    var result = document.getElementById('result').value;
-    var calculatedResult = eval(result);
-    document.getElementById('result').value = calculatedResult;
-  }
-</script>
-
+        function clearResult() {
+            document.getElementById("result").value = "";
+        }
+    </script>
 </body>
 </html>
